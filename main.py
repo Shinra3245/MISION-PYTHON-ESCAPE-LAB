90ab0a7798f8681ed6120d4c00a623ecc3673ae5
Shinra3245 <weplay3245@gmail.com>
2026-07-16 20:16:15 -0600

    feat/etapa 7 completada, Inicializacion de la etapa del juego, opciones elegibles para el jugador, acertijos y problemas declarados, escape de el laboratorio declarado y ejecutado con exito

--- main.py
+++ main.py
@@ -78,5 +78,73 @@ inventario = [] #inventario vacio
 print("Tu inventario está vacío. A medida que explores, encontrarás objetos que te ayudarán a escapar.")
 print(inventario)
 
+# -----------------
+# ETAPA 7
+# Darle la capacidad al videojuego de reiniciar y no termine hasta decidirlo
+# -----------------
 
+while juego_activo == True:
 
+    print()
+    print("=" * 50)
+    print("LABORATORIO - RECEPCIÓN")
+    print("=" * 50)
+    
+    print("1. Explorar la recepción")
+    print("2. Ver tu inventario")
+    print("3. Intentar abrir la puerta de salida")
+    print("4. Salir del juego")
+    
+    opcion = int(input("Selecciona una opción: "))
+    if opcion == 1:
+        print()
+        print("Exploras la recepción y encuentras una llave en el escritorio.")
+        
+        if not llave:
+            print("Encuentras una llave y la agregas a tu inventario.")
+            llave = True
+            inventario.append("Llave") # Agregamos la llave al inventario
+            
+        elif not targeta:
+            print("Encuentras una tarjeta de acceso en un cajón y la agregas a tu inventario.")
+            targeta = True
+            inventario.append("Tarjeta de acceso")
+            
+        elif not codigo_descubierto:
+            print("Encuentras un papel con un código secreto escrito en él y lo agregas a tu inventario.")
+            codigo_descubierto = True
+            inventario.append("Código secreto")
+        
+        else:
+            print("No hay más objetos para encontrar en la recepción.")
+    
+    elif opcion == 2:
+        print()
+        print(f"Tu inventario contiene: {inventario}")
+        if len(inventario) == 0:
+            print("Tu inventario está vacío. Explora para encontrar objetos.")
+        else:
+            for objeto in inventario:
+                print(f"- {objeto}")
+    
+    elif opcion == 3:
+        print()
+        print("Intentas abrir la puerta de salida...")
+        if not llave:
+            print("Necesitas una llave para abrir la puerta.")
+        elif not targeta:
+            print("Necesitas una tarjeta de acceso para abrir la puerta.")
+        elif not codigo_descubierto:
+            print("Necesitas un código secreto para abrir la puerta.")
+        
+        if llave == True and targeta == True and codigo_descubierto == True:
+            print("¡Felicidades! Has utilizado la llave, la tarjeta de acceso y el código secreto para abrir la puerta de salida.")
+            print("Has escapado del laboratorio con éxito. ¡Fin del juego!")
+            juego_activo = False
+            
+    elif opcion == 4:
+        print("Has decidido salir del juego. ¡Hasta la próxima!")
+        juego_activo = False
+            
+    
+    
